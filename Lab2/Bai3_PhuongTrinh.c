d8ad19b50ec6fe95e2c10f4efd94e344a012e807
hoangduy2608 <hahoangduy268@gmail.com>
2026-07-14 18:42:42 +0700

    Hoan Thanh Lab 2 Bai 3

--- Lab2/Bai3_PhuongTrinh.c
+++ Lab2/Bai3_PhuongTrinh.c
@@ -9,6 +9,9 @@ int main() {
     printf("Nhap b: ");
     scanf("%f", &b);
 
+    printf("So a: %.2f\n", a);
+
+    printf("So b: %.2f\n", b);
     float pTBN = -b/a;
     printf("Phuong trinh co nghiem: x = %.2f\n", pTBN);
 
