16c5b455278bb05b3188c03ebf0a822e4173e66c
hoangduy2608 <hahoangduy268@gmail.com>
2026-08-12 20:02:34 +0700

    Lab7: Hoan thanh Chuc nang 4 - Chuyen doi so Thap phan sang Nhi phan bang Chuoi

--- Lab7/Main_Menu_Lab7.c
+++ Lab7/Main_Menu_Lab7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void demNguyenAmPhuAm(char str[50]) {
+void demNguyenAmPhuAm(char str[]) {
     int demNA = 0;
     int demPA = 0;
     strlwr(str);
@@ -24,6 +24,46 @@ void kiemTraDangNhap(char user[], char pass[]) {
     else 
         printf("Username hoac Password khong chinh xac! \n");
 }
+
+void sapXepChuoi(char s[][50]) {
+    char temp[50];
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (strcmp(s[i], s[j]) > 0) {
+                strcpy(temp, s[i]);
+                strcpy(s[i], s[j]);
+                strcpy(s[j], temp);
+            }
+        }
+    }
+
+    printf("\nDanh sach sau khi sap xep (Alphabet):\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d. %s\n", i + 1, s[i]);
+    }
+}
+
+void thapPhanSangNhiPhan(int n) {
+    int i = 0;
+    char nhiPhan[50];
+
+    if (n == 0) {
+        printf("So nhi phan: 0\n");
+        return;
+    }
+    while (n > 0) {
+        nhiPhan[i] = (n % 2) + '0';
+        n = n / 2;
+        i++;
+    }
+    nhiPhan[i] = '\0';
+    printf("So nhi phan: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", nhiPhan[j]);
+    }
+    printf("\n");
+}
+
 int main() {
     int choice;
     do {
@@ -34,7 +74,7 @@ int main() {
         printf("1. Dem nguyen am va phu am trong chuoi \n");
         printf("2. Dang nhap he thong (User & Password) \n");
         printf("3. Sap xep danh sach chuoi theo thu tu Alphabet \n");
-        printf("4. Chuoi doi so thap phan sang nhi phan(chuoi) \n");
+        printf("4. Chuyen doi so thap phan sang nhi phan(chuoi) \n");
         printf("5. Thoat chuong trinh \n");
         printf("+-------------------------------+\n");
         printf("Xin moi chon chuc nang(1-5): ");
@@ -62,7 +102,21 @@ int main() {
 
                 kiemTraDangNhap(user, pass);
                 break;
-
+            case 3:
+                char s[5][50];
+                for(int i = 0; i < 5; i++) {
+                    printf("Nhap vao chuoi thu %d: ", i+1);
+                    fgets(s[i], sizeof(s[i]), stdin);
+                    s[i][strcspn(s[i], "\n")] = '\0';
+                }
+                sapXepChuoi(s);
+                break;
+            case 4:
+                int n;
+                printf("Nhap so nguyen duong: ");
+                scanf("%d", &n);
+                getchar();
+                thapPhanSangNhiPhan(n);
         }
     }while(choice != 5);
 
