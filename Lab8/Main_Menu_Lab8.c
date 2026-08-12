56d332d8ee289e9ce96e4c713309c6559f62aadd
hoangduy2608 <hahoangduy268@gmail.com>
2026-08-12 20:42:54 +0700

    Lab8: Hoan thanh Chuc nang 1 - Nhap va Xuat danh sach sinh vien

--- Lab8/Main_Menu_Lab8.c
+++ Lab8/Main_Menu_Lab8.c
@@ -8,6 +8,31 @@ struct SinhVien {
     float diemTB;
 };
 
+void nhapXuatSV(struct SinhVien mangSV[], int *n) {
+    for (int i = 0; i < *n; i++) {
+        printf("Nhap thong tin sinh vien thu %d: \n", i+1);
+        printf("Nhap ma so sinh vien (MSSV): ");
+        fgets(mangSV[i].mssv, sizeof(mangSV[i].mssv), stdin);
+        mangSV[i].mssv[strcspn(mangSV[i].mssv, "\n")] = '\0';
+
+        printf("Nhap ho va ten : ");
+        fgets(mangSV[i].tenSV, sizeof(mangSV[i].tenSV), stdin);
+        mangSV[i].tenSV[strcspn(mangSV[i].tenSV, "\n")] = '\0';
+
+        printf("Nhap nganh hoc: ");
+        fgets(mangSV[i].nganhHoc, sizeof(mangSV[i].nganhHoc), stdin);
+        mangSV[i].nganhHoc[strcspn(mangSV[i].nganhHoc, "\n")] = '\0';
+        
+        printf("Nhap diem TB: ");
+        scanf("%f", &mangSV[i].diemTB);
+        getchar();
+    }
+
+    printf("Danh sach sinh vien: \n");
+    for (int i = 0; i < *n; i++) {
+        printf("Sinh vien thu %d: MSSV: %s | Ho ten: %s | Nganh hoc: %s | Diem TB: %.2f \n", i+1, mangSV[i].mssv, mangSV[i].tenSV, mangSV[i].nganhHoc, mangSV[i].diemTB);
+    }
+}
 int main() {
     int choice;
     do {
@@ -27,7 +52,12 @@ int main() {
 
         switch(choice) {
             case 1:
-
+                int n;
+                struct SinhVien mangSV[100];
+                printf("Nhap vao so luong sinh vien: ");
+                scanf("%d", &n);
+                getchar();
+                nhapXuatSV(mangSV, &n);
         }
     }while(choice != 5);
 
